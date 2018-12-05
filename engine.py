78a95476c5886bd73f6e85cd74369b23c4d9e976
Unknown <sautaxgaming@gmail.com>
2018-12-05 19:31:27 +0100

    ajout de commentaires et de documentation sur engine.py

--- engine.py
+++ engine.py
@@ -2,6 +2,10 @@ import pygame
 
 
 def doubleArraygen(x, y):
+    """
+    Générateur de tableau à deux entrées rempli de base de "0"
+    usage : doubleArraygen(largeur (x) , longueur (y) )
+    """
     array = []
     for i in range(x):
         line = []
@@ -11,19 +15,43 @@ def doubleArraygen(x, y):
     return array
 
 
-class Map:
+class Carte:
+    """
+    Objet Carte : permet de charger une carte à partir d'un fichier, de l'afficher et d'en créer une.
+    Méthodes :
+        - Carte(path, mode="load", dimensions=(10, 10), tileSize=64)  : création de l'objet carte
+            path : chemin d'accès ,
+            mode : [load / new / edit] charger / créer / éditer
+            dimensions : taille en x et y
+        - carte.save(): sauvegare de la carte à l'emplacement spécifié lors de la création
+        - carte.edit(x,y,textureIndex) : self.grid[x][y] = textureIndex
+            textureIndex : chaîne de caractères
+        - carte.render(textures) : renvoi un objet surface de la librairie Pygame avec le rendu de la carte
+            textures : dictionnaire des différentes textures référencés dans le fichier carte,
+                il doit contenir au moins une surface pour l'index "0" 
+                exemple : textures =   {"0": SurfaceEau,
+                                        "1": SurfaceTerre, 
+                                        "arbre": SurfaceArbre} 
+    """
     grid = []
-    width = 0
-    height = 0
-    tileSize = 64
-    path = ""
+    width = 0  # taille suivant x
+    height = 0  # taille suivant y
+    tileSize = 64  # lageur et longuer d'une texture de la carte
+    path = ""  # chemin du fichier
 
     def __init__(self, path, mode="load", dimensions=(10, 10), tileSize=64):
+        """
+        __init__(path, mode="load", dimensions=(10, 10), tileSize=64)  : création de l'objet carte
+            path : chemin d'accès ,
+            mode : ["load" / "new" / "edit" ] charger / créer / éditer , charger (load) par défaut
+            dimensions : taille en x et y
+        """
         self.grid = []
         self.tileSize = tileSize
         self.path = path
+        # détection du mode
         if mode == "load":
-            with open(path, "r") as file:
+            with open(path, "r") as file:  # ouverture du fichier
                 self.grid = []
                 line = file.readline()
                 (self.width, self.height) = line.split()
@@ -33,12 +61,12 @@ class Map:
                 while line != "":
                     self.grid.append(line.split())
                     line = file.readline()
-        elif mode == "new":
+        elif mode == "new":  # création d'une nouvelle carte
             self.grid = doubleArraygen(dimensions[0], dimensions[1])
             self.width = dimensions[0]
             self.height = dimensions[1]
         elif mode == "edit":
-            with open(path, "r") as file:
+            with open(path, "r") as file:  # ouverture du fichier
                 self.grid = []
                 line = file.readline()
                 (self.width, self.height) = line.split()
@@ -53,7 +81,8 @@ class Map:
             raise ValueError("invalid mode")
 
     def save(self):
-        with open(self.path, "w") as file:
+        """Sauvegare de la carte à l'emplacement spécifié lors de la création"""
+        with open(self.path, "w") as file:  # sauvegarder à l'emplacement défini dans path
             file.write("{} {}\n".format(self.width, self.height))
             for line in self.grid:
                 for c in line:
@@ -64,12 +93,20 @@ class Map:
         self.grid[x][y] = textureIndex
 
     def render(self, textures):
+        """carte.render(textures) : renvoi un objet surface de la librairie Pygame avec le rendu de la carte
+            textures : dictionnaire des différentes textures référencés dans le fichier carte,
+                il doit contenir au moins une surface pour l'index "0" 
+                exemple : textures =   {"0": SurfaceEau,
+                                        "1": SurfaceTerre, 
+                                        "arbre": SurfaceArbre} 
+        """
         surface = pygame.Surface(
-            (self.width*self.tileSize, self.height*self.tileSize))
+            (self.width*self.tileSize, self.height*self.tileSize))  # surface où va être rendu la carte
         x = 0
         for l in self.grid:
             y = 0
             for p in l:
+                # ajoute la texture à l'index p aux coordonnées x et y
                 surface.blit(textures[p], (x, y))
                 y += self.tileSize
             x += self.tileSize
