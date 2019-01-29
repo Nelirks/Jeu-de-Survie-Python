71527387b3b54b22e275c946d7e1911336564084
Nelirks <raphael.lesbros@laposte.net>
2019-01-29 17:12:16 +0100

    Modification de la classe items

--- items.py
+++ items.py
@@ -12,8 +12,9 @@ class Item:
     """
     nom = ""
     description = ""
+    quantity = 0
 
-    def __init__(self, nom, texture, description=""):
+    def __init__(self, nom, quantity, texture, description=""):
         """
         crée un objet Item pour être utilisé dans l'inventaire,
         il est préférable d'utiliser un objet enfant du type adapté (ex: potion, arme)
@@ -21,10 +22,11 @@ class Item:
         self.nom = nom
         self.description = description
         self.texture = texture.convert_alpha()
+        self.quantity = quantity
 
     def render(self):
         """
-        retourne une surface avec la l'item
+        retourne une surface avec l'item
         """
         surface = pygame.Surface(CONST_TileItemSize, CONST_TileItemSize)
         surface.blit(self.texture, (0, 0))
@@ -70,53 +72,58 @@ class MagicWeapon(Weapon):
         super().applyDamage(entity)
 '''
 class ItemContainer :
-    itemtype = []
-    itemquantity = []
+    items = []
     
     def __init__(self, size) : #Création de deux listes liste ayant une longueur size
         for n in range(size) :
-            self.itemtype.append("0")
-            self.itemquantity.append(0)
+            self.items.append("0")
 
     def sizeincrease(self ,sizeup): #Augmentation de la longueur des listes en cas d'augmentation de l'inventaire
         for n in range (sizeup):
-            self.itemtype.append("0")
-            self.itemquantity.append(0)
+            self.items.append("0")
 
-    def additem(self, name, quantity, place): #Ajout d'un item en vérifiant que la case n'est pas vide, si elle est vide, renvoie l'item précedent et sa quantité
-        if self.itemquantity[place] != 0 :
-            if self.itemtype == name :
-                self.itemquantity[place] += quantity
-                return (1,"It worked !")
-        else : 
-            oldname = self.itemtype[place]
-            oldquantity = self.itemquantity[place]
-            self.itemtype[n] = name
-            self.itemquantity = quantity
-            return (oldname, oldquantity)
+    def additem(self, itemadded, place): #Ajout d'un item en vérifiant que la case n'est pas vide, si elle est vide, renvoie l'item précedent et sa quantité
+        if self.items[place].quantity != 0 :
+            if self.items[place].nom == itemadded.nom :
+                self.items[place].quantity += itemadded.quantity
+                return ("It worked !")
+            else : 
+                olditem = self.items[place]
+                self.items[place] = itemadded
+                return (olditem)
+        else :
+            self.items[place] = itemadded
 
 
     def removeitem(self, place, mode) : #Permet le retrait d'item, avec trois modes, half, one et all, et renvoie le type et a quantité d'item retirés
         if mode == "all" :
-            name = self.itemtype[place]
-            quantity = self.itemquantity[place]
-            self.itemquantity[place] = 0
-            self.itemtype[place] = "0"
-        if mode == "half":
-            name = self.item[place]
-            quantity = ceil(self.item[place]/2)
-            self.itemquantity[place] -= quantity
-            if self.itemquantity[place] == 0:
-                self.itemtype[place] = "0"
+            itemremoved = self.items[place]
+            self.items[place] ="0"
+        if mode == "half" :
+            itemremoved = self.items[place]
+            itemremoved.quantity = ceil(itemremoved.quantity /2)
+            self.items[place].quantity -= itemremoved.quantity
+            if self.items[place].quantity == 0:
+                self.items[place] = "0"
         if mode == "one" :
-            name = self.item[place]
-            quantity = 1
-            self.itemquantity[place] -= quantity
-            if self.itemquantity[place] == 0:
-                self.itemtype[place] = "0"
-        return name, quantity
+            itemremoved = self.items[place]
+            itemremoved.quantity = 1
+            self.items[place].quantity -= itemremoved.quantity
+            if self.items[place].quantity == 0:
+                self.items[place] = "0"
+        return itemremoved
 
-    def render(self, largeur) :
-        itemsperline = largeur//32
-        itempercolumn = ceil(len(self.itemquantity)/itemperline)
-        surfacefinale = pygame.display.set_mode((itemperline,itempercolumn))
+
+
+    def render(self, largeur) : #Crée une surface avec tous les items dans un rectangle de largeur donnée en pixel
+        itemperline = largeur//32
+        itempercolumn = ceil(len(len(self.items))/itemperline)
+        surfacefinale = pygame.Surface((itemperline*32, itemperline*32))
+        for y in range (itempercolumn) :
+            for x in range (itemperline) :
+                if self.items[x+y*itemperline] != "0" :
+                    surfacefinale.blit(self.items[x+y*itemperline].texture,(x*32,y*32))
+        return surfacefinale
+
+
+
