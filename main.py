e09d9d190219477c8cdbd2a7abc348debc882879
sautax <nilsponsard@gmail.com>
2019-02-05 17:49:25 +0100

    optimisations

--- main.py
+++ main.py
@@ -6,23 +6,16 @@ import os
 
 game = engine.Engine((1280, 800), framerate=100)
 
-haut = pygame.image.load(os.path.join("assets", "haut.png"))
-bas = pygame.image.load(os.path.join("assets", "bas.png"))
-droite = pygame.image.load(os.path.join("assets", "droite.png"))
-gauche = pygame.image.load(os.path.join("assets", "gauche.png"))
-
-ptextures = [droite, bas, gauche, haut]
-
 pygame.key.set_repeat(50, 1)
 
-wall = engine.Carte(os.path.join("assets", "levels", "test"), mode="load")
+wall = engine.Carte(os.path.join("assets", "levels", "test"),
+                    mode="load")  # objet carte
 
+game = engine.Engine((wall.width, wall.height), framerate=50)  # fenêtre
 
-game = engine.Engine((wall.width, wall.height), framerate=50)
+back = wall.renderSurface()  # surface carte
 
-back = wall.renderSurface()
-
-p1 = entities.Player(0, 0, "1")
+p1 = entities.Player(0, 0, "1")  # joueur
 
 while game.state != 0:
     events = game.runEvents()
