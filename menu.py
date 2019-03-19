a0e65585a611f32341f7a1dfc6bb21bd60ca47d7
sautax <sautaxgaming@gmail.com>
2019-03-19 20:49:50 +0100

    added stars

--- menu.py
+++ menu.py
@@ -1,9 +1,58 @@
 import pygame
 import main
 import os
+import random
 import engine
+
+
+class Star:
+
+    def __init__(self, position, size, speed=(-10, 0)):
+        self.speed = speed
+        self.x = position[0]
+        self.y = position[1]
+        self.surface = pygame.surface.Surface((size*2, size))
+        self.surface.fill((255, 255, 255))
+
+    def render(self, surface):
+        surface.blit(self.surface, (self.x, self.y))
+
+    def update(self):
+
+        self.x += self.speed[0]
+        self.y += self.speed[1]
+
+
+class Stars:
+    """
+    une nuée d'étoiles défilant
+    """
+
+    liste = []
+    line = 5
+    frame = 0
+
+    def render(self, surface):
+        if self.frame % 8 == 0:
+            width = surface.get_width()
+            height = surface.get_height()
+            for i in range(self.line):
+                x = width + random.randint(-10, 10)
+                y = random.randint(10, height-10)
+                self.liste.append(Star((x, y), random.randint(1, 5)))
+        i = 0
+        for s in self.liste:
+            s.render(surface)
+            s.update()
+            if s.x < 0:
+                self.liste.pop(i)
+            else:
+                i += 1
+        self.frame += 1
+
+
 game = engine.Engine((1280, 720),
-                     (1280, 720), framerate=65)  # fenêtre 1:1 pour les boutons
+                     (1280, 720), framerate=100)  # fenêtre 1:1 pour les boutons
 game.state = 0
 
 playEvent = pygame.USEREVENT + 2
@@ -11,11 +60,11 @@ creditsEvent = pygame.USEREVENT + 3
 
 
 playButton = engine.Button(
-    (10, 10), (200, 50),  "Jouer", playEvent, fontSize=60)
+    (10, 10), (200, 50),  "Jouer", playEvent, fontSize=60, focusedBackground=(100, 100, 100))
 creditsButton = engine.Button(
-    (10, 70), (110, 40),  "Crédits", creditsEvent, fontSize=40)
+    (10, 70), (110, 40),  "Crédits", creditsEvent, fontSize=40, focusedBackground=(100, 100, 100))
 closeButton = engine.Button(
-    (10, 120), (250, 40),  "Retour au bureau", pygame.QUIT, fontSize=40)
+    (10, 120), (250, 40),  "Retour au bureau", pygame.QUIT, fontSize=40, focusedBackground=(100, 100, 100))
 
 
 def mainMenu():
@@ -23,13 +72,15 @@ def mainMenu():
     game.state = 0
     game.changeMode((1280, 720), (1280, 720))
     events = []
+    stars = Stars()
     pygame.mixer_music.load(os.path.join("assets", "music", "main_Menu.mp3"))
     pygame.mixer_music.play(-1)
     while game.state == 0:
         playButton.update(events)
         creditsButton.update(events)
         closeButton.update(events)
-        game.screen.fill((50, 50, 0))
+        game.screen.fill((0, 0, 0))
+        stars.render(game.screen)
         game.screen.blit(playButton.render(), playButton.position)
         game.screen.blit(creditsButton.render(), creditsButton.position)
         game.screen.blit(closeButton.render(), closeButton.position)
